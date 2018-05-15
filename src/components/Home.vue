eed6bebde9fb4745feefced626959790594d8858
chenzemin <chen.zemin@bw.com>
2018-05-15 15:21:41 +0800

    矿机列表添加搜索框

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,7 +18,7 @@
     <el-row>
       <el-tabs type="border-card">
         <el-tab-pane label="告警矿机">
-          <miner-table-list :table-data="errorTableData"></miner-table-list>
+          <miner-table-list :table-data="errorTableData" @search="searchError"></miner-table-list>
         </el-tab-pane>
         <el-tab-pane label="所有矿机">
           <miner-table-list :table-data="fullTableData"></miner-table-list>
@@ -39,18 +39,44 @@ export default {
         {
           id: '12',
           type: 'bwminer2.60a',
+          ip: '192.168.6.13',
           status: 'error',
           location: 'XX省XX市XX区XX街道XX号'
         },
         {
           id: '14',
           type: 'bwminer2.60b',
+          ip: '192.168.6.14',
           status: 'error',
           location: 'XX省XX市XX区XX街道123号'
         },
         {
           id: '15',
           type: 'bwminer2.60c',
+          ip: '192.168.6.15',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道456号'
+        }
+      ],
+      fakeErrorTableData: [
+        {
+          id: '12',
+          type: 'bwminer2.60a',
+          ip: '192.168.6.13',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道XX号'
+        },
+        {
+          id: '14',
+          type: 'bwminer2.60b',
+          ip: '192.168.6.14',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道123号'
+        },
+        {
+          id: '15',
+          type: 'bwminer2.60c',
+          ip: '192.168.6.15',
           status: 'error',
           location: 'XX省XX市XX区XX街道456号'
         }
@@ -59,30 +85,72 @@ export default {
         {
           id: '12',
           type: 'bwminer2.60a',
+          ip: '192.168.6.13',
           status: 'error',
           location: 'XX省XX市XX区XX街道XX号'
         },
         {
           id: '14',
           type: 'bwminer2.60b',
+          ip: '192.168.6.14',
           status: 'error',
           location: 'XX省XX市XX区XX街道123号'
         },
         {
           id: '15',
           type: 'bwminer2.60c',
+          ip: '192.168.6.15',
           status: 'error',
           location: 'XX省XX市XX区XX街道456号'
         },
         {
           id: '16',
           type: 'bwminer2.60d',
+          ip: '192.168.6.16',
           status: 'active',
           location: 'XX省XX市XX区XX街道456号'
         },
         {
           id: '17',
           type: 'bwminer2.60e',
+          ip: '192.168.6.17',
+          status: 'active',
+          location: 'XX省XX市XX区XX街道456号'
+        }
+      ],
+      fakeFullTableData: [
+        {
+          id: '12',
+          type: 'bwminer2.60a',
+          ip: '192.168.6.13',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道XX号'
+        },
+        {
+          id: '14',
+          type: 'bwminer2.60b',
+          ip: '192.168.6.14',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道123号'
+        },
+        {
+          id: '15',
+          type: 'bwminer2.60c',
+          ip: '192.168.6.15',
+          status: 'error',
+          location: 'XX省XX市XX区XX街道456号'
+        },
+        {
+          id: '16',
+          type: 'bwminer2.60d',
+          ip: '192.168.6.16',
+          status: 'active',
+          location: 'XX省XX市XX区XX街道456号'
+        },
+        {
+          id: '17',
+          type: 'bwminer2.60e',
+          ip: '192.168.6.17',
           status: 'active',
           location: 'XX省XX市XX区XX街道456号'
         }
@@ -92,6 +160,21 @@ export default {
   components: {
     HashrateSpline: HashrateSpline,
     MinerTableList: MinerTableList
+  },
+  methods: {
+    searchError (text) {
+      let res = []
+      if (/^\s*$/.test(text)) {
+        res = this.fakeErrorTableData
+      } else {
+        this.fakeErrorTableData.forEach((v) => {
+          if (v.ip.indexOf(text) >= 0) {
+            res.push(v)
+          }
+        })
+      }
+      this.errorTableData = res
+    }
   }
 }
 </script>
--- src/components/MinerTableList.vue
+++ src/components/MinerTableList.vue
@@ -1,20 +1,34 @@
 <template>
-  <el-table :data="tableData" stripe style="width: 100%">
-    <!-- <el-table-column prop="num" label="序号"></el-table-column> -->
-    <el-table-column type="index"></el-table-column>
-    <el-table-column prop="type" label="型号"></el-table-column>
-    <el-table-column prop="status" label="状态">
-      <template slot-scope="scope">
-        <span :class="{'clr-danger': scope.row.status=='error'}">{{ scope.row.status }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="location" label="位置"></el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button @click="viewMiner(scope.row.id)" type="text" size="small">查看</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div class="clearfix">
+      <div class="fl">
+        <el-input placeholder="请输入搜索内容" v-model="searchText">
+          <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+        </el-input>
+      </div>
+      <div class="fr">
+        <el-button type="primary" @click="reset">重置</el-button>
+        <el-button type="primary" @click="exportTable">导出</el-button>
+      </div>
+    </div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <!-- <el-table-column prop="num" label="序号"></el-table-column> -->
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="type" label="型号"></el-table-column>
+      <el-table-column prop="ip" label="IP"></el-table-column>
+      <el-table-column prop="status" label="状态">
+        <template slot-scope="scope">
+          <span :class="{'clr-danger': scope.row.status=='error'}">{{ scope.row.status }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="location" label="位置"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="viewMiner(scope.row.id)" type="text" size="small">查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -26,11 +40,23 @@ export default {
   },
   data () {
     return {
+      searchText: ''
     }
   },
   methods: {
     viewMiner (id) {
       this.$router.push('/miner-detail/' + id)
+    },
+    search () {
+      let text = this.searchText
+      this.$emit('search', text)
+    },
+    reset () {
+      this.searchText = ''
+      this.search()
+    },
+    exportTable () {
+      console.log(this.tableData)
     }
   }
 }
